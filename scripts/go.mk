f64b469a4316101c59d869236c8c7cecc855258e
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-11-05 14:28:21 -0300

    some adjustments

--- scripts/go.mk
+++ scripts/go.mk
@@ -1,5 +1,5 @@
 define build_go
 	env GOARCH=arm64 GOOS=linux go build -ldflags="-s -w" -o bin/$1/bootstrap $1/main.go
 	chmod +x bin/$1/bootstrap
-	zip -j bin/$2.zip bin/$1/bootstrap
+	zip -j bin/$2 bin/$1/bootstrap
 endef
