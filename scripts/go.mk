a9a548184a4827c9895d23e5fdd8ce958bc28a0e
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-11-05 14:02:20 -0300

    test fix

--- scripts/go.mk
+++ scripts/go.mk
@@ -1,22 +1,5 @@
-define build_go_binary
-	@if [ -z $(1) ]; then \
-		echo "executable path is required"; \
-		exit 1; \
-	fi
-	@if [ -z $(2) ]; then \
-		echo "zip name is required"; \
-		exit 1; \
-	fi
-	BOOTSTRAP_PATH="bin/$(1)/bootstrap"; \
-	export GO111MODULE=on; \
-	export CGO_ENABLED=1; \
-	export GOARCH=arm64; \
-	if [ `uname` = "Darwin" ]; then \
-		export GOOS=darwin; \
-	else \
-		export GOOS=linux; \
-	fi; \
-	go build -ldflags="-s -w" -o $$BOOTSTRAP_PATH $(1)/main.go; \
-	chmod +x $$BOOTSTRAP_PATH; \
-	zip -j bin/$(2).zip $$BOOTSTRAP_PATH
+define build_go
+	env GOARCH=arm64 GOOS=linux go build -ldflags="-s -w" -o bin/$1/bootstrap $1/main.go
+	chmod +x bin/$1/bootstrap
+	zip -j bin/$2.zip bin/$1/bootstrap
 endef
